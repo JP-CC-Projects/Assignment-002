2ccffebf656c9e4e9a5a6b720644159ed8b5d601
JP-CC-Projects <joeparkcc@gmail.com>
2023-05-15 17:01:31 -0500

    removed code-testing sysout msg

--- Main.java
+++ Main.java
@@ -10,7 +10,6 @@ public class Main {
         int guessNum = Integer.parseInt(sc.nextLine());
         int guessesLeft = 4;
 
-        System.out.println(randNum);
         while(guessNum != randNum && guessesLeft > 0){
             if(guessNum > 100 || guessNum < 1){
                 System.out.println("Your guess is not between 1 and 100, please try again");
